d9ee775fd1cda9a8bb18212f6345f2bf8a086efa
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-05-26 12:19:56 -0400

    Built teacher and student classes

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+attr_accessor :knowledge
+
+def initialize
+  @knowledge = []
+end
+
+def learn(knowledge_string)
+  self.knowledge << knowledge_string
+end
+
+
 
 end
